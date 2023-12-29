8573355de07ffb2ae1706d526b3d8e6eb955351c
Manezeu Patricia <pmanezeu@gmail.com>
2023-12-29 14:20:20 +0100

    Added models

---
+++ app/models/expense.rb
@@ -0,0 +1,40 @@
+class Expense < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    has_many :category_expenses
+    has_many :categories, through: :category_expenses
+
+    validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 100 }
+    validates :description, length: { minimum: 3, maximum: 400 }
+    validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+    def self.last_five_expenses_grouped_by_category
+        Expense.joins(:categories)
+               .order(created_at: :desc)
+               .limit(5)
+               .group('categories.id')
+               .select('categories.name as category_name, SUM(expenses.amount) as total_amount, expenses.*')
+    end
+
+    def self.expenses_by_category(category_id)
+        Expense.joins(:categories)
+               .where(category_id: category_id)
+               .order(created_at: :desc)
+    end
+
+    def self.current_month_expenses
+        start_of_month = Date.today.beginning_of_month
+        end_of_month = Date.today.end_of_month
+    
+        Expense.joins(:categories)
+               .where(created_at: start_of_month..end_of_month)
+               .group('categories.id')
+               .select('categories.name as category_name, SUM(expenses.amount) as total_amount, expenses.*')
+    end
+
+    def self.expenses_in_period(start_date, end_date)
+        Expense.joins(:categories)
+               .where(created_at: start_date..end_date)
+               .group('categories.id')
+               .select('categories.name as category_name, SUM(expenses.amount) as total_amount, expenses.*')
+    end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+    has_many :categories
+    has_many :expenses, foreign_key: 'author_id'
+    
+    validates :name, presence: true, uniqueness: true
+end
