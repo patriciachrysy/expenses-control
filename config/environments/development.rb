0d6205507b927cad281123ca76ddbbcd8256557f
Manezeu Patricia <pmanezeu@gmail.com>
2023-12-30 11:58:38 +0100

    Added devise and cancancan with configurations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,15 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => '8d258605915ce0',
+    :password => '1048eb098e0d7e',
+    :address => 'sandbox.smtp.mailtrap.io',
+    :host => 'sandbox.smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
 end
